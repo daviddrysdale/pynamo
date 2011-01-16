fe3593ed1032d0e2e8fe4dd54c623be907182d0c
David Drysdale <dmd@lurklurk.org>
2011-01-16 01:53:02 +0000

    add failure cases

--- test_dynamo.py
+++ test_dynamo.py
@@ -4,7 +4,9 @@ import unittest
 
 from framework import Framework, reset
 from history import History
+
 import dynamo1
+import dynamo
 
 class SimpleTestCase(unittest.TestCase):
     """Test simple Dynamo function"""
@@ -43,6 +45,68 @@ class SimpleTestCase(unittest.TestCase):
         b.put('K2', None, 17)
         Framework.schedule()
         print History.ladder(spacing=14)
+
+    def test_simple_put_fail1(self):
+        for _ in range(6):
+            dynamo1.DynamoNode()
+        a = dynamo1.DynamoClientNode('a')
+        destnode = random.choice(dynamo1.DynamoNode.nodelist)
+        a.put('K1', None, 1, destnode=destnode)
+        # Fail at the forwarding node before it gets a chance to forward
+        destnode.fail()
+        Framework.schedule()
+        print History.ladder()
+
+    def test_simple_put_fail2(self):
+        for _ in range(6):
+            dynamo1.DynamoNode()
+        a = dynamo1.DynamoClientNode('a')
+        destnode = random.choice(dynamo1.DynamoNode.nodelist)
+        a.put('K1', None, 1, destnode=destnode)
+        # Fail at the forwarding node after it gets a chance to forward
+        Framework.schedule(1)
+        destnode.fail()
+        Framework.schedule()
+        print History.ladder()
+
+    def test_simple_put_fail3(self):
+        for _ in range(6):
+            dynamo1.DynamoNode()
+        a = dynamo1.DynamoClientNode('a')
+        a.put('K1', None, 1)
+        # Fail the second node in the preference list
+        pref_list = dynamo1.DynamoNode.chash.find_nodes('K1', 3)
+        Framework.schedule(1)
+        pref_list[1].fail()
+        Framework.schedule()
+        a.get('K1')
+        Framework.schedule()
+        print History.ladder()
+
+    def test_simple_put_fail4(self):
+        for _ in range(6):
+            dynamo1.DynamoNode()
+        a = dynamo1.DynamoClientNode('a')
+        a.put('K1', None, 1)
+        # Fail the second and third node in the preference list
+        pref_list = dynamo1.DynamoNode.chash.find_nodes('K1', 3)
+        Framework.schedule(1)
+        pref_list[1].fail()
+        pref_list[2].fail()
+        Framework.schedule()
+        print History.ladder()
+    
+    def test_client_retry(self):
+        for _ in range(6):
+            dynamo.DynamoNode()
+        a = dynamo.DynamoClientNode('a')
+        destnode = random.choice(dynamo.DynamoNode.nodelist)
+        a.put('K1', None, 1, destnode=destnode)
+        # Fail at the forwarding node before it gets a chance to forward
+        destnode.fail()
+        Framework.schedule()
+        print "\n".join(["%s: %s" % (e[0],str(e[1])) for e in History.history])
+        print History.ladder()
 
 if __name__ == "__main__":
     for ii in range(1, len(sys.argv)-1): # pragma: no cover
