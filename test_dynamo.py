a5d47db02f942f81eb6817a6ff8a88874eb9ecf9
David Drysdale <dmd@lurklurk.org>
2011-01-23 00:31:40 +0000

    +

--- test_dynamo.py
+++ test_dynamo.py
@@ -97,44 +97,52 @@ class SimpleTestCase(unittest.TestCase):
         self.put_fail_nodes23(dynamo1)
         print History.ladder()
     def test_put2_fail_nodes23(self):
-        self.put_fail_nodes23(dynamo2)
-        # Force nodes that are of interest in put2_fail_nodes23_2 to be included in the history
-        pref_list = dynamo2.DynamoNode.chash.find_nodes('K1', 5)
+        (_, pref_list) = self.put_fail_nodes23(dynamo2)
+        # Force nodes that are of interest in put2_fail_nodes23_[234] to be included in the history
         print History.ladder(force_include=pref_list)
     def put_fail_nodes23(self, cls):
         for _ in range(6): cls.DynamoNode()
         a = cls.DynamoClientNode('a')
         # Fail the second and third node in the preference list
-        pref_list = cls.DynamoNode.chash.find_nodes('K1', 3)
+        pref_list = cls.DynamoNode.chash.find_nodes('K1', 5)
         a.put('K1', None, 1, destnode=pref_list[0])
         Framework.schedule(1)
         pref_list[1].fail()
         pref_list[2].fail()
         Framework.schedule(timers_to_process=2)
-        return a, pref_list[0]
+        return a, pref_list
     
     def test_put2_fail_nodes23_2(self):
-        (a, destnode) = self.put_fail_nodes23(dynamo2)
+        """Show second request for same key skipping failed nodes"""
+        (a, pref_list) = self.put_fail_nodes23(dynamo2)
+        destnode = pref_list[0]
         from_line = len(History.history)
         a.put('K1', None, 2, destnode=destnode)
         Framework.schedule()
-        print History.ladder(start_line=from_line)
+        print History.ladder(force_include=pref_list, start_line=from_line)
 
     def test_put2_fail_nodes23_3(self):
-        for _ in range(6): dynamo99.DynamoNode()
-        a = dynamo99.DynamoClientNode('a')
-        # Fail the second and third node in the preference list
-        pref_list = dynamo99.DynamoNode.chash.find_nodes('K1', 3)
-        a.put('K1', None, 1, destnode=pref_list[0])
-        Framework.schedule(1, timers_to_process=0)
-        pref_list[1].fail()
-        pref_list[2].fail()
-        Framework.schedule(timers_to_process=2)
-        print History.ladder() # @@
-        return
-        a.put('K1', None, 2, destnode=pref_list[0])
-        Framework.schedule(timers_to_process=3)
-        print History.ladder()
+        """Show PingReq failing"""
+        (a, pref_list) = self.put_fail_nodes23(dynamo99)
+        from_line = len(History.history)
+        Framework.schedule(timers_to_process=3) 
+        print History.ladder(force_include=pref_list, start_line=from_line)
+        
+    def test_put2_fail_nodes23_4(self):
+        """Show PingReq recovering, and a subsequent Put returning to the original preference list"""
+        (a, pref_list) = self.put_fail_nodes23(dynamo99)
+        destnode = pref_list[0]
+        from_line = len(History.history)
+        a.put('K1', None, 2, destnode=destnode)
+        Framework.schedule(timers_to_process=10)
+        from_line = len(History.history)
+        pref_list[1].recover()
+        pref_list[2].recover()
+        Framework.schedule(timers_to_process=15)
+        a.put('K1', None, 3, destnode=pref_list[0])
+        Framework.schedule(timers_to_process=5)
+        print History.ladder(force_include=pref_list, start_line=from_line)
+        # print History.ladder() # @@@@ staggered start to ... lines
     
 if __name__ == "__main__":
     for ii in range(1, len(sys.argv)-1): # pragma: no cover
