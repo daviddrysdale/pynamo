76652b418db482b93a19ff5b76ef4c81e675929c
David Drysdale <dmd@lurklurk.org>
2011-05-23 00:06:05 +0000

    fill out dict tests

--- merkledict.py
+++ merkledict.py
@@ -17,11 +17,38 @@ class MerkleDict(MerkleTree, dict):
                                      (ii, len(pair)))
                 self[pair[0]] = pair[1]
 
+    # Methods that set values
     def __setitem__(self, key, value):
         return dict.__setitem__(self, key, value)
 
+    def update(self, other=(), **kwds):
+        if hasattr(other, "keys"):
+            for key in other.keys():
+                self[key] = other[key]
+        else:
+            for key, value in other:
+                self[key] = value
+        for key, value in kwds.items():
+            self[key] = value
+
+    # Methods that remove values
     def __delitem__(self, key):
         return dict.__delitem__(self, key)
+
+    def clear(self):
+        return dict.clear(self)
+
+    __marker = object()
+
+    def pop(self, key, default=__marker):
+        if default is self.__marker:
+            return dict.pop(self, key)
+        else:
+            return dict.pop(self, key, default)
+
+    def popitem(self):
+        return dict.popitem(self)
+
 
 # -----------IGNOREBEYOND: test code ---------------
 import unittest
@@ -58,6 +85,22 @@ class MerkleContainerTest(unittest.TestCase):
         self.assertFalse('b' in d2)
         self.assertFalse('c' in d2)
 
+    def test002(self):
+        d1 = MerkleDict({'a': 1, 'b': 2, 'c': 3})
+        d2 = MerkleDict((('a', 1), ('b', 2), ('c', 3)))
+        self.assertEqual(d1, d2)
+        self.assertEqual(d1.pop('a'), 1)
+        self.assertEqual(d1.pop('b'), 2)
+        self.assertEqual(d1.pop('x', 'yy'), 'yy')
+        self.assertEqual(d1.popitem(), ('c', 3))
 
+        d2.update({'x': 8, 'y': 9})
+        self.assertEqual(set(('a','b','c','x','y')),
+                         set(d2.keys()))
+        d2.update((('u', 10), ('v', 11), ('w', 12)))
+        self.assertEqual(set(('a','b','c','u','v','w','x','y')),
+                         set(d2.keys()))
+
+        
 if __name__ == "__main__":
     unittest.main()
