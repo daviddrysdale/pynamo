38686b06c86748001efca7ef6d649489ead8f932
David Drysdale <dmd@lurklurk.org>
2011-07-19 06:12:32 +0000

    incomplete start on Merkle node

---
+++ merklemessages.py
@@ -0,0 +1,25 @@
+"""Messages between Merkle nodes"""
+from message import Message, ResponseMessage
+
+class MerkleRequestMessage(Message):
+    """Base class for Merkle request messages; all include the tree config info"""
+    def __init__(self, from_node, to_node, depth, min_key, max_key, msg_id=None):
+        Message.__init__(self, from_node, to_node, msg_id=msg_id)
+        self.depth = depth
+        self.min_key = min_key
+        self.max_key = max_key
+    def __str__(self):
+        return "%s |%s| [%s,%s)" % (Message.__str__(self), self.depth, self.min_key, self.max_key)
+
+
+class MerkleResponseMessage(ResponseMessage):
+    """Base class for Merkle response messages; all include the tree config info"""
+    def __init__(self, req):
+        ResponseMessage.__init__(self, req)
+        self.depth = depth
+        self.min_key = req.min_key
+        self.max_key = req.max_key
+    def __str__(self):
+        return "%s |%s| [%s,%s)" % (Message.__str__(self), self.depth, self.min_key, self.max_key)
+
+
---
+++ merklenode.py
@@ -0,0 +1,22 @@
+"""Node implementing a Merkle tree"""
+import logging
+
+import logconfig
+from node import Node
+from timer import Timer
+from framework import Framework
+from merklemessages import @@@
+from merkle import MerkleTree
+
+logconfig.init_logging()
+_logger = logging.getLogger('merkle')
+
+# PART merklenode
+class DynamoNode(Node):
+
+    def __init__(self):
+        Node.__init__(self)
+
+    def rcvmsg(self, msg):
+        if isinstance(msg, ClientPut): self.rcv_clientput(msg)
+        else: raise TypeError("Unexpected message type %s", msg.__class__)
